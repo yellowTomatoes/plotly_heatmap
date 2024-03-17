e01d5e3123457df42739582964cd7b245cf17093
yellowTomatoes <dylan.carter1@myokanagan.bc.ca>
2024-03-17 15:21:17 -0700

    On branch master  Changes to be committed: 	new file:   createRibbons.js 	modified:   histogram2d.html 	modified:   pallete.js

---
+++ createRibbons.js
@@ -0,0 +1,103 @@
+var ibgData =[["meter_id","channel_id","uom","date_dtm","toutier","nvl","read_dt","metername","readstatus"],
+    ["100001",1,"KWH","2019-01-16","Mid-peak",6.19,"2019-01-16 00:00","501 N Rexford Dr - ELECTRIC (100001 - 1000001)",""],
+    ["100001",1,"KWH","2019-01-16","Off-peak",28.85,"2019-01-16 01:00","501 N Rexford Dr - ELECTRIC (100001 - 1000001)",""],
+    ["100001",1,"KWH","2019-01-16","On-peak",10.66,"2019-01-16 02:00","501 N Rexford Dr - ELECTRIC (100001 - 1000001)",""],
+    ["100001",1,"KWH","2019-01-17","Mid-peak",19.82,"2019-01-17 03:00","501 N Rexford Dr - ELECTRIC (100001 - 1000001)",""],
+    ["100001",1,"KWH","2019-01-17","Off-peak",10.10,"2019-01-17 02:00","501 N Rexford Dr - ELECTRIC (100001 - 1000001)",""],
+    ["100001",1,"KWH","2019-01-17","On-peak",18.22,"2019-01-17 00:00","501 N Rexford Dr - ELECTRIC (100001 - 1000001)",""],
+    ["100001",1,"KWH","2019-01-18","Mid-peak",14.17,"2019-01-18 04:00","501 N Rexford Dr - ELECTRIC (100001 - 1000001)",""],
+    ["100001",1,"KWH","2019-01-18","Off-peak",14.88,"2019-01-18 03:00","501 N Rexford Dr - ELECTRIC (100001 - 1000001)",""],
+    ["100001",1,"KWH","2019-01-18","On-peak",10.80,"2019-01-18 06:00","501 N Rexford Dr - ELECTRIC (100001 - 1000001)",""],
+    ["100001",1,"KWH","2019-01-19","Off-peak",49.31,"2019-01-19 01:00","501 N Rexford Dr - ELECTRIC (100001 - 1000001)",""],
+    ["100001",1,"KWH","2019-01-20","Off-peak",35.21,"2019-01-20 03:00","501 N Rexford Dr - ELECTRIC (100001 - 1000001)",""],
+    ["100001",1,"KWH","2019-01-21","Mid-peak",8.06,"2019-01-21 06:00","501 N Rexford Dr - ELECTRIC (100001 - 1000001)",""],
+    ["100001",1,"KWH","2019-01-21","Off-peak",8.81,"2019-01-21 02:00","501 N Rexford Dr - ELECTRIC (100001 - 1000001)",""],
+    ["100001",1,"KWH","2019-01-21","On-peak",10.76,"2019-01-21 01:00","501 N Rexford Dr - ELECTRIC (100001 - 1000001)",""],
+    ["100001",1,"KWH","2019-01-22","Mid-peak",14.36,"2019-01-22 00:00","501 N Rexford Dr - ELECTRIC (100001 - 1000001)",""],
+    ["100001",1,"KWH","2019-01-22","Off-peak",14.05,"2019-01-22 09:00","501 N Rexford Dr - ELECTRIC (100001 - 1000001)",""],
+    ["100001",1,"KWH","2019-01-22","On-peak",11.49,"2019-01-22 11:00","501 N Rexford Dr - ELECTRIC (100001 - 1000001)",""],
+    ["100001",1,"KWH","2019-01-23","Mid-peak",10.67,"2019-01-23 10:00","501 N Rexford Dr - ELECTRIC (100001 - 1000001)",""],
+    ["100001",1,"KWH","2019-01-23","Off-peak",12.80,"2019-01-23 04:00","501 N Rexford Dr - ELECTRIC (100001 - 1000001)",""],
+    ["100001",1,"KWH","2019-01-23","On-peak",16.89,"2019-01-23 07:00","501 N Rexford Dr - ELECTRIC (100001 - 1000001)",""],
+    ["100001",1,"KWH","2019-01-24","Mid-peak",11.77,"2019-01-24 13:00","501 N Rexford Dr - ELECTRIC (100001 - 1000001)",""],
+    ["100001",1,"KWH","2019-01-24","Off-peak",19.73,"2019-01-24 18:00","501 N Rexford Dr - ELECTRIC (100001 - 1000001)",""],
+    ["100001",1,"KWH","2019-01-24","On-peak",11.37,"2019-01-24 10:00","501 N Rexford Dr - ELECTRIC (100001 - 1000001)",""],
+    ["100001",1,"KWH","2019-01-25","Mid-peak",9.44,"2019-01-25 00:00","501 N Rexford Dr - ELECTRIC (100001 - 1000001)",""],
+    ["100001",1,"KWH","2019-01-25","Off-peak",16.97,"2019-01-25 01:00","501 N Rexford Dr - ELECTRIC (100001 - 1000001)",""],
+    ["100001",1,"KWH","2019-01-25","On-peak",16.01,"2019-01-25 02:00","501 N Rexford Dr - ELECTRIC (100001 - 1000001)",""],
+    ["100001",1,"KWH","2019-01-26","Off-peak",48.76,"2019-01-26 04:00","501 N Rexford Dr - ELECTRIC (100001 - 1000001)",""],
+    ["100001",1,"KWH","2019-01-27","Off-peak",43.31,"2019-01-27 03:00","501 N Rexford Dr - ELECTRIC (100001 - 1000001)",""],
+    ["100001",1,"KWH","2019-01-28","Mid-peak",4.97,"2019-01-28 00:00","501 N Rexford Dr - ELECTRIC (100001 - 1000001)",""],
+    ["100001",1,"KWH","2019-01-28","Off-peak",12.06,"2019-01-28 06:00","501 N Rexford Dr - ELECTRIC (100001 - 1000001)",""],
+    ["100001",1,"KWH","2019-01-28","On-peak",11.33,"2019-01-28 02:00","501 N Rexford Dr - ELECTRIC (100001 - 1000001)",""],
+    ["100001",1,"KWH","2019-01-29","Mid-peak",8.48,"2019-01-29 00:00","501 N Rexford Dr - ELECTRIC (100001 - 1000001)",""],
+    ["100001",1,"KWH","2019-01-29","Off-peak",13.85,"2019-01-29 00:00","501 N Rexford Dr - ELECTRIC (100001 - 1000001)",""],
+    ["100001",1,"KWH","2019-01-29","On-peak",15.24,"2019-01-29 01:00","501 N Rexford Dr - ELECTRIC (100001 - 1000001)",""],
+    ["100001",1,"KWH","2019-01-30","Mid-peak",6.25,"2019-01-30 02:00","501 N Rexford Dr - ELECTRIC (100001 - 1000001)",""],
+    ["100001",1,"KWH","2019-01-30","Off-peak",10.95,"2019-01-30 00:00","501 N Rexford Dr - ELECTRIC (100001 - 1000001)",""],
+    ["100001",1,"KWH","2019-01-30","On-peak",8.10,"2019-01-30 02:00","501 N Rexford Dr - ELECTRIC (100001 - 1000001)",""]];
+
+function getRibbons(color){
+	
+	var x1 = [];
+	var y1 = [];
+	var z1 = [];
+	
+	var x2 = [];
+	var y2 = [];
+	var z2 = [];
+	
+	var x3 = [];
+	var y3 = [];
+	var z3 = [];
+	
+	for(var i=1;i<ibgData.length;i++){
+		y1.push(i);
+		x1.push(ibgData[i][5]);
+		z1.push([1,1]);
+		
+		y2.push(i);
+		x2.push(ibgData[i][5] * Math.random());
+		z2.push([2,2]);
+		
+		y3.push(i);
+		x3.push(ibgData[i][5] * Math.random());
+		z3.push([3,3]);
+	}
+	
+	var trace1 = {
+		x:x1,
+		y:y1,
+		z:z1,
+		
+		colorscale : color,
+		type: "surface",
+		showscale: false  //not sure what this does.
+		
+	};
+	var trace2 = {
+		x:x2,
+		y:y2,
+		z:z2,
+		
+		colorscale : color,
+		type: "surface",
+		showscale: false  //not sure what this does.
+		
+	};
+		var trace3 = {
+		x:x3,
+		y:y3,
+		z:z3,
+		
+		colorscale : color,
+		type: "surface",
+		showscale: false  //not sure what this does.
+		
+	};
+	
+	console.log(x1);
+	console.log(y1);
+	return [trace1,trace2,trace3];
+	
+}
